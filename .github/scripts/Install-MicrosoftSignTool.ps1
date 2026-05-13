2d73c98e72a2c9338fe27d16808a1bfe76a0742c
David Hammett <david@cloudedskies.com>
2026-05-13 14:34:16 +0100

    Fixed issue with install script not reporting error codes

--- .github/scripts/Install-MicrosoftSignTool.ps1
+++ .github/scripts/Install-MicrosoftSignTool.ps1
@@ -14,7 +14,7 @@ if ((Test-Path $windowsSdkFilePath) -eq $false) {
 	    Invoke-WebRequest -Uri $windowsSdkDownloadUrl -Method Get -OutFile $windowsSdkFilePath -ErrorAction Stop
     } catch {
         Write-Host "Failed to download the Windows SDK!. $($_.Exception.Message)" -ForegroundColor Red
-        exit
+        exit 1
     }
 }
 
@@ -23,7 +23,7 @@ if ((Test-Path $visualCppRedistFilePath) -eq $false) {
 	    Invoke-WebRequest -Uri $visualCppRuntimeUrl -Method Get -OutFile $visualCppRedistFilePath -ErrorAction Stop
     } catch {
         Write-Host "Failed to download the Visual C++!. $($_.Exception.Message)" -ForegroundColor Red
-        exit
+        exit 1
     }
 }
 
@@ -32,33 +32,38 @@ if ((Test-Path $officeSipX86FilePath) -eq $false) {
 	    Invoke-WebRequest -Uri $officeSipX86DownloadUrl -Method Get -OutFile $officeSipX86FilePath -ErrorAction Stop
     } catch {
         Write-Host "Failed to download the Office SIP!. $($_.Exception.Message)" -ForegroundColor Red
-        exit
+        exit 1
     }
 }
 
 $process = (Start-Process -FilePath $windowsSdkFilePath -ArgumentList "/installpath","$windowsSdkPath","/features","OptionId.SigningTools","/q","/norestart" -PassThru -Wait)
 if ($process.ExitCode -ne 0) {
-    Write-Host "$windowsSdkFilePath returned error code $errorCode" -ForegroundColor Red
+    Write-Host "$windowsSdkFilePath returned error code $($process.ExitCode)" -ForegroundColor Red
+    exit $process.ExitCode
 }
 
 $process = (Start-Process -FilePath $visualCppRedistFilePath -ArgumentList "/quiet","/norestart" -PassThru -Wait)
 if ($process.ExitCode -ne 0) {
-    Write-Host "$visualCppRedistFilePath returned error code $errorCode" -ForegroundColor Red
+    Write-Host "$visualCppRedistFilePath returned error code $($process.ExitCode)" -ForegroundColor Red
+    exit $process.ExitCode
 }
 
 $process = (Start-Process -FilePath $officeSipX86FilePath -ArgumentList "/extract:$officeSipPath","/quiet","/norestart" -PassThru -Wait)
 if ($process.ExitCode -ne 0) {
-    Write-Host "$officeSipX86FilePath returned error code $errorCode" -ForegroundColor Red
+    Write-Host "$officeSipX86FilePath returned error code $($process.ExitCode)" -ForegroundColor Red
+    exit $process.ExitCode
 }
 
 $process = (Start-Process -FilePath $regsvr32FilePath -ArgumentList "/s","$officeSipPath\msosip.dll" -PassThru -Wait)
 if ($process.ExitCode -ne 0) {
-    Write-Host "Registering $officeSipPath\msosip.dll returned error code $errorCode" -ForegroundColor Red
+    Write-Host "Registering $officeSipPath\msosip.dll returned error code $($process.ExitCode)" -ForegroundColor Red
+    exit $process.ExitCode
 }
 
 $process = (Start-Process -FilePath $regsvr32FilePath -ArgumentList "/s","$officeSipPath\msosipx.dll" -PassThru -Wait)
 if ($process.ExitCode -ne 0) {
-    Write-Host "Registering $officeSipPath\msosipx.dll returned error code $errorCode" -ForegroundColor Red
+    Write-Host "Registering $officeSipPath\msosipx.dll returned error code $($process.ExitCode)" -ForegroundColor Red
+    exit $process.ExitCode
 }
 
 # New-SelfSignedCertificate -Type CodeSigningCert -KeyAlgorithm RSA -KeyLength 2048 -KeyExportPolicy Exportable -Subject "E=david.hammett@domain.net,CN=David Hammett" -CertStoreLocation "Cert:\CurrentUser\My"
