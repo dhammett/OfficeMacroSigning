e726cb5b0df3c304716bf7c2176233bf1b10f757
David Hammett <david@cloudedskies.com>
2026-05-13 15:09:01 +0100

    Removed Windows SDK as it maybe installed already

--- .github/scripts/Install-MicrosoftSignTool.ps1
+++ .github/scripts/Install-MicrosoftSignTool.ps1
@@ -1,15 +1,15 @@
-$windowsSdkDownloadUrl = "https://go.microsoft.com/fwlink/?linkid=2361309"
+#$windowsSdkDownloadUrl = "https://go.microsoft.com/fwlink/?linkid=2361309"
 $officeSipX86DownloadUrl = "https://download.microsoft.com/download/c53e473c-3060-4ee9-ac5c-0ddbbeced4e5/OfficeSips_x86_16-0-19416-43425.exe"
 $visualCppRuntimeUrl = "https://download.microsoft.com/download/C/6/D/C6D0FD4E-9E53-4897-9B91-836EBA2AACD3/vcredist_x86.exe"
 
-$windowsSdkFilePath = "$($env:TEMP)\winsdksetup.exe"
+#$windowsSdkFilePath = "$($env:TEMP)\winsdksetup.exe"
 $officeSipX86FilePath = "$($env:TEMP)\OfficeSips_x86_16-0-19416-43425.exe"
 $visualCppRedistFilePath = "$($env:TEMP)\vcredist_x86.exe"
 $regsvr32FilePath = "$($env:SYSTEMROOT)\System32\regsvr32.exe"
 $officeSipPath = "C:\OfficeSIP"
-$windowsSdkPath = "C:\WindowsSDK"
+#$windowsSdkPath = "C:\WindowsSDK"
 
-if ((Test-Path $windowsSdkFilePath) -eq $false) {
+<#if ((Test-Path $windowsSdkFilePath) -eq $false) {
     try {
 	    Invoke-WebRequest -Uri $windowsSdkDownloadUrl -Method Get -OutFile $windowsSdkFilePath -ErrorAction Stop
         Unblock-File -Path $windowsSdkFilePath -ErrorAction Stop
@@ -17,7 +17,7 @@ if ((Test-Path $windowsSdkFilePath) -eq $false) {
         Write-Host "Failed to download the Windows SDK!. $($_.Exception.Message)" -ForegroundColor Red
         exit 1
     }
-}
+}#>
 
 if ((Test-Path $visualCppRedistFilePath) -eq $false) {
     try {
@@ -39,11 +39,11 @@ if ((Test-Path $officeSipX86FilePath) -eq $false) {
     }
 }
 
-$process = (Start-Process -FilePath $windowsSdkFilePath -ArgumentList "/installpath","$windowsSdkPath","/features","OptionId.SigningTools","/q","/norestart" -PassThru -Wait)
+<#$process = (Start-Process -FilePath $windowsSdkFilePath -ArgumentList "/installpath","$windowsSdkPath","/features","OptionId.SigningTools","/q","/norestart" -PassThru -Wait)
 if ($process.ExitCode -ne 0) {
     Write-Host "$windowsSdkFilePath returned error code $($process.ExitCode)" -ForegroundColor Red
     exit $process.ExitCode
-}
+}#>
 
 $process = (Start-Process -FilePath $visualCppRedistFilePath -ArgumentList "/quiet","/norestart" -PassThru -Wait)
 if ($process.ExitCode -ne 0) {
@@ -68,6 +68,8 @@ if ($process.ExitCode -ne 0) {
     Write-Host "Registering $officeSipPath\msosipx.dll returned error code $($process.ExitCode)" -ForegroundColor Red
     exit $process.ExitCode
 }
+
+$env:PATH
 
 # New-SelfSignedCertificate -Type CodeSigningCert -KeyAlgorithm RSA -KeyLength 2048 -KeyExportPolicy Exportable -Subject "E=david.hammett@domain.net,CN=David Hammett" -CertStoreLocation "Cert:\CurrentUser\My"
 # OffSign.bat "C:\WindowsSDK\bin\10.0.28000.0\x86\" "sign /f C:\Users\Administrator\Documents\CodeSigning.pfx /p Password1 /fd SHA256 /tr http://timestamp.digicert.com /td SHA256" "verify /pa" "C:\Users\Administrator\Documents\Button Macro Test Signed.xlsm"
